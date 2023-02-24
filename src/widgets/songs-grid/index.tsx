e3d68762235089208c2595563b9ecb5cc4469807
Gavryk <gvozdyk23@gmail.com>
2023-02-24 23:26:41 +0200

    ui: play / pause button rfc

--- src/widgets/songs-grid/index.tsx
+++ src/widgets/songs-grid/index.tsx
@@ -4,6 +4,7 @@ import { UICard, UIGrid, UITypography } from '../../components';
 import { SkeletonCard } from '../../components/ui-card/skeleton-card';
 import { SongProp } from '../../redux/slices/player/types';
 import { settingsSelector } from '../../redux/slices/settings/selector';
+import { useAppDispatch } from '../../redux/store';
 import styles from './styles.module.scss';
 
 interface SongsProps {
@@ -11,9 +12,16 @@ interface SongsProps {
 }
 
 export const Songs: React.FC<SongsProps> = ({ list }) => {
+	const dispatch = useAppDispatch();
 	const { isLoaded } = useSelector(settingsSelector);
 
-	const songsItems = list.map((song) => <UICard {...song} key={song.key} />);
+	const handleStartStop = (song: SongProp) => {
+		console.log(song);
+	};
+
+	const songsItems = list.map((song) => (
+		<UICard {...song} key={song.key} clickHandler={() => handleStartStop(song)} />
+	));
 	const skeleton = [...new Array(9)].map((_, index) => <SkeletonCard key={index} />);
 
 	return isLoaded === 'error' ? (
