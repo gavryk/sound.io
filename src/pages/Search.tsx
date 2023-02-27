33c1f40e2a25211354dabd1c6966560f8f9223b1
Gavryk <gvozdyk23@gmail.com>
2023-02-27 16:19:49 +0200

    redux: search page and search redux slice

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchSongsBySearch } from '../redux/slices/player/asyncSongs';
 import { playerSelector } from '../redux/slices/player/selector';
-import { HitsTrackProp } from '../redux/slices/player/types';
 import { useAppDispatch } from '../redux/store';
 import { TopBlock } from '../widgets';
 import { Songs } from '../widgets/songs-grid';
@@ -11,18 +10,16 @@ import { Songs } from '../widgets/songs-grid';
 export const Search = () => {
 	const dispatch = useAppDispatch();
 	const { q = '' } = useParams();
-	const { searchResult } = useSelector(playerSelector);
+	const { songs } = useSelector(playerSelector);
 
 	useEffect(() => {
 		dispatch(fetchSongsBySearch(q));
 	}, [dispatch, q]);
 
-	const searchSongs = searchResult.tracks?.hits.map((song: HitsTrackProp) => song?.track);
-
 	return (
 		<>
 			<TopBlock title={`Search ${q}`} />
-			<Songs list={searchSongs} />
+			<Songs list={songs} />
 		</>
 	);
 };
