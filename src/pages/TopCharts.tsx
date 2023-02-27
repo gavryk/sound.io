2dcf9e1b15f1223bde8e309b539b0417fcfd307b
Gavryk <gvozdyk23@gmail.com>
2023-02-27 16:27:10 +0200

    ui: top charts page

--- src/pages/TopCharts.tsx
+++ src/pages/TopCharts.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { fetchTopCharts } from '../redux/slices/player/asyncSongs';
+import { playerSelector } from '../redux/slices/player/selector';
+import { useAppDispatch } from '../redux/store';
 import { TopBlock } from '../widgets';
+import { Songs } from '../widgets/songs-grid';
 
 export const TopCharts = () => {
-  return (
-    <>
-      <TopBlock title="Top Charts" />
-    </>
-  );
+	const dispatch = useAppDispatch();
+	const { songs } = useSelector(playerSelector);
+
+	useEffect(() => {
+		dispatch(fetchTopCharts());
+	}, [dispatch]);
+
+	return (
+		<>
+			<TopBlock title="Top Charts" />
+			<Songs list={songs} />
+		</>
+	);
 };
